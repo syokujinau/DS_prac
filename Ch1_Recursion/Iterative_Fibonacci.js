def4e2d83ee7ca2e5e6e34bc133c80859ac30946
syokujinau <npr286rx@rocketmail.com>
2017-07-02 17:20:28 +0800

    test

---
+++ Ch1_Recursion/Iterative_Fibonacci.js
@@ -0,0 +1,20 @@
+//Iterative Fibonacci
+
+
+function Fib(n){
+	if(n==0) return 0;
+	else if (n==1) return 1;
+	else {
+		var a=0,b=1,c;
+		for (var i = 2; i <= n; i++) {
+			c=a+b;
+			a=b;
+			b=c;
+		}
+		return c;
+	} 
+}
+
+for (var j = 0; j <= 6; j++) {
+	 console.log("F"+j+" = "+Fib(j));
+}

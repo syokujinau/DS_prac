6f52e8d7add1a26df0552b0b79fe9685a0659b34
linyuchien <npr286rx@rocketmail.com>
2017-07-08 00:12:18 +0800

    pointer func

--- ReviewPointer/CallByPointer.c
+++ ReviewPointer/CallByPointer.c
@@ -4,12 +4,26 @@ void mul2(int *x){
     *x=*x*2;
 }
 
+void Print_hello_world(){
+    printf("hello world \n");
+}
+
+void do_func_10_times(void (*func)()){
+    for (int i = 0; i < 10; i++)
+    {
+        func();
+    }
+}
+
 int main(){
     int a=10;
     printf("%d\n",a);
     printf("multiply 2\n",a);
     mul2(&a);
     printf("%d\n",a);
+
+
+    do_func_10_times(Print_hello_world);
 
     return 0;
 }

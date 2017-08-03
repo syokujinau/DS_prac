9f73409f6109f8488191ca3d7e5b16d96fae7e63
linyuchien <npr286rx@rocketmail.com>
2017-08-04 00:36:41 +0800

    debugging :cat2:

--- Ch3_Stack&Queue/Stack_linked-list_Ver2.c
+++ Ch3_Stack&Queue/Stack_linked-list_Ver2.c
@@ -8,55 +8,60 @@ struct Node {
   struct Node* Next;
 };
 typedef struct Node* PtrToNode; //Node型態指標 別名PtrToNode
-typedef PtrToNode Stack;        //即PtrToNode  特別用來指向stack的Top 
+typedef PtrToNode Stack;        //即PtrToNode  特別用來指向stack的Top
 
 
-void Push(int, Stack); 
+void Push(int, Stack);
 void Pop(Stack);
 
 int main(){
   printf("OK\n");
-  Stack s1;  //Node 型態的指標變數 名稱叫s1 指向該stack的top
+  Stack s1=NULL;  //Node 型態的指標變數 名稱叫s1 指向該stack的top
   Push(1,s1);
+  Push(2,s1);
   Pop(s1);
-  
-  
+
   return 0;
 }
 
 
-/* 
+/*
  *Push函式
- *parameter:(push data, pointer to top)
+ *parameter:(push data, pointer to top of stack)
 */
 void Push(int x, Stack s){
-  PtrToNode Tmp;
-  Tmp = (PtrToNode*)malloc(sizeof(PtrToNode)); //??
-  if(Tmp == NULL){
-    printf("out of Memory!\n");
+  if(s == NULL){
+    s       = (PtrToNode)malloc(sizeof(PtrToNode));
+    s->data = x;    printf("s->data:%d\n",s->data);      //!!!!!!!!
+    s->Next = NULL;
   }
   else{
-    Tmp->data = x;
+    PtrToNode Tmp;
+    Tmp = (PtrToNode)malloc(sizeof(PtrToNode)); //??
+    if(Tmp == NULL){
+        printf("out of Memory!\n");
+    }
+    Tmp->data = x;  printf("Tmp->data:%d\n",Tmp->data); //!!!!!!!!
     Tmp->Next = s;
     s         = Tmp;
   }
+  printf("push : %d\n",x);
 }
 
-/* 
+/*
  *Pop函式
  *parameter:(pointer to top)
 */
 void Pop(Stack s){
-  PtrToNode Tmp;
-  int item;
   if(s == NULL){
     printf("Stack is empty!\n");
   }
   else{
+    PtrToNode Tmp;
     Tmp  = s;
-    item = s->data;
-    printf("pop %d\n",item);
+    printf("pop : %d\n",Tmp->data);
     s    = s->Next;
     free(Tmp);
   }
 }
+

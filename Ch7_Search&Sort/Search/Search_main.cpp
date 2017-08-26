e3d292e86e058a2e8d18e8e49cb844fcfa681ba2
syokujinau <npr286rx@rocketmail.com>
2017-08-26 16:54:51 +0800

    bug in searh

--- Ch7_Search&Sort/Search/Search_main.cpp
+++ Ch7_Search&Sort/Search/Search_main.cpp
@@ -38,7 +38,7 @@ int LinearSearch(record ary[],int size, int k){
   //cout << endl << "Searching target key is : " << k <<endl;
   int index;
 	ary[size].key = k;//崗哨，ary[size].key放指定鍵值
-	printData(ary,10);
+	//printData(ary,10);
 	for (index = 0; ary[index].key != k; index++){
 		//index遞增直到碰到指定鍵值，則該index值即為遇搜尋資料所在
 		//若都沒搜尋到則碰到崗哨時的鍵值會等於size  回傳-1代表Not Found
@@ -72,6 +72,7 @@ int BinarySearch(record ary[],int left,int right, int k){ //recursive版本
 	if(left <= right){
 		middle = (left+right)/2;
 		if (k == ary[middle].key){
+		  cout << "Found!\n";
 			return middle;
 		}
 		else if(k < ary[middle].key){ //找左半邊
@@ -85,7 +86,7 @@ int BinarySearch(record ary[],int left,int right, int k){ //recursive版本
 }
 
 void SetData(record ary[], int size){
-	int key[size] = {4,7,8,9,11,13,22,34};
+	int key[size] = {1,5,8,9,10,11,20,24};
 	char data[size] = {'A','F','G','E','J','Q','W','X'};
 	for(int i=0; i<size; i++){
 		ary[i].key  = key[i];
@@ -99,8 +100,3 @@ void printData(record* ary,int capacity){
   		cout << ary[i].key << "\t" << ary[i].data <<endl;
   	}
 }
-
-
-
-
-

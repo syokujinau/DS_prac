c322d508c18eb1b7d543e6c72d8e6be790c0d849
linyuchien <npr286rx@rocketmail.com>
2017-09-04 18:14:40 +0800

    min

---
+++ Ch1_Recursion/minimum.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int min(int A[], int size){
+  if (size == 1){
+    return A[1];
+  }
+  else{
+    
+    if(A[size] > min(A,size-1))
+      return min(A,size-1);
+    else
+      return A[size];
+  }
+}
+
+
+
+int main(){ 
+  int ary[]={33,44,55,11,66};
+  int x = min(ary,5);
+  cout <<x<<endl;
+}
+

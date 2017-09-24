65c3a30b17140f7edead56ef350985e54ccd6535
linyuchien <npr286rx@rocketmail.com>
2017-09-25 01:25:30 +0800

    node type constructor

--- Ch.6_Graph/DFS.cpp
+++ Ch.6_Graph/DFS.cpp
@@ -2,6 +2,7 @@
 #define MAX 8  //頂點總數
 using namespace std;
 
+/*宣告相鄰矩陣*/
 int adjMatrix[MAX][MAX] = {
 //V1 V2 V3 V4 V5 V6 V7 V8
  { 0, 1, 1, 0, 0, 0, 0, 0 }, //V1
@@ -15,16 +16,25 @@ int adjMatrix[MAX][MAX] = {
 };
 
 /*宣告頂點之相鄰串列節點型態*/
-typedef struct node{
+struct node{
   int vertex; //頂點編號
   struct node* link;
-}Node;
+  node(int v){ //node建構子
+    vertex = v;
+    link = NULL;//預設指向NULL
+  }
+};  
+typedef struct node Node;
 
 /*
 [注意]頂點編號減掉1才是所對應的index
 */
 
-/*深度優先搜尋*/
+/*
+圖形物件
+宣告後須先建立相鄰串列(傳入相鄰矩陣資料)
+才能執行DFS
+*/
 class Graph { 
   private:
     Node* adjlist[MAX];   //相鄰串列 
@@ -33,31 +43,26 @@ class Graph {
     void build_adjlist(int A[][MAX]);
     void print_adjlist();
     void dfs(int v);
-    Node* search_last(Node* vertex);
+    Node* search_last(Node* vertex); //用於建立相鄰串列
 };
 
 /*由adjMatrix建立相鄰串列，因為是Row-major，所以需要先定義col數*/
 void Graph::build_adjlist(int A[][MAX]){
-  Node *node, *lastnode;
+  Node *t, *lastnode;
   int vi,vj,e;
   /*初始化拜訪陣列與相鄰串列*/
   for(vi=0; vi < MAX ; vi++){
     visited[vi] = false;
-    adjlist[vi] = new Node;  //Node型態陣列成員
-    adjlist[vi]->vertex = vi+1; //index=0放1號頂點，依此類推
-    adjlist[vi]->link = NULL;//初始指向NULL
+    adjlist[vi] = new Node(vi+1);  //Node型態陣列成員，index=0放1號頂點，依此類推
   }
   /*一一讀入每個row，將該row存成串列*/
   for(vi=0; vi < MAX; vi++){
     for(vj=0; vj < MAX; vj++){
-      //fin >> e;
       e = A[vi][vj];
       if(e == 1){ //若e=1(有邊存在)
-        node = new Node; //新增一節點
-        node->vertex   = vj+1;  //此時vj+1值即為相鄰之頂點編號
-        node->link     = NULL;
+        t = new Node(vj+1); //新增一節點，此時vj+1值即為相鄰之頂點編號
         lastnode       = search_last(adjlist[vi]);  //lastnode指向該串列最後一個節點
-        lastnode->link = node;
+        lastnode->link = t;
       }
     }
   }
